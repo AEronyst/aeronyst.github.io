337ebe6d1e718be71209b856a5c7de513c8542f0
AEronyst <aeronyst@gmail.com>
2026-05-27 15:20:17 +0900

    feat: 언어 선택에 따라 YouTube 플레이어 언어 연동

--- sections-b.js
+++ sections-b.js
@@ -118,7 +118,7 @@ window.CategoryItem = function CategoryItem({ item, lang, onImageClick }) {
     return React.createElement("div", { className: "cat-item" },
       React.createElement("div", { className: "cat-item-media" },
         React.createElement("iframe", {
-          src: `https://www.youtube-nocookie.com/embed/${item.youtubeId}?rel=0&playsinline=1`,
+          src: `https://www.youtube-nocookie.com/embed/${item.youtubeId}?rel=0&playsinline=1&hl=${lang}`,
           width: "100%", height: "100%",
           frameBorder: "0",
           allow: "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen",
